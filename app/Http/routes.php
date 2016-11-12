770a2450ff6cb54a04ae1d866ea71ea51d7fe7b8
kawashima <nazonohito51@hotmail.com>
2016-11-12 16:51:50 +0900

    add tweet index routing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', function() {
-    return 'Hello World!!';
-});
+Route::get('tweet', 'TweetController@index');
 
