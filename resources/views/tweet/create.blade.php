e5cca9bc13f38b842c331de1d64c4634ad1c1dbe
kawashima <nazonohito51@hotmail.com>
2016-11-19 21:08:59 +0900

    use bootstrap design

--- resources/views/tweet/create.blade.php
+++ resources/views/tweet/create.blade.php
@@ -3,15 +3,23 @@
 <head>
     <meta charset="utf-8">
     <title>Twitter風アプリ</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
 <form action="{!! url('tweet') !!}" method="post">
-    {!! csrf_field() !!}
+    <div class="container">
+        <div class="page-header">
+            <h1>ツイート一覧</h1>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                {!! csrf_field() !!}
 
-    <label for="body">ツイート本文：</label>
-    <input type="text" name="body"/>
-
-    <button type="submit">投稿する</button>
+                <input type="text" name="body" class="form-control" placeholder="ツイート本文を入力してください。"/>
+                <button type="submit" class="btn btn-primary">投稿する</button>
+            </div>
+        </div>
+    </div>
 </form>
 </body>
 </html>
--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -3,17 +3,29 @@
 <head>
     <meta charset="utf-8">
     <title>Twitter風アプリ</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
-    <a href="{!! url('/tweet/create') !!}">新しいツイートを投稿する</a>
-    <table>
-        <tbody>
-            @foreach($tweets as $tweet)
-            <tr>
-                <td>{{ $tweet->body }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="container">
+        <div class="page-header">
+            <h1>ツイート新規投稿</h1>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <a href="{!! url('/tweet/create') !!}" class="btn btn-primary">新しいツイートを投稿する</a>
+            </div>
+            <div class="col-md-10">
+                <table class="table">
+                    <tbody>
+                        @foreach($tweets as $tweet)
+                        <tr>
+                            <td>{{ $tweet->body }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
