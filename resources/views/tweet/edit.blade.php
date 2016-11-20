c88762f14ecbcd3f8144b07071552393aceb5d3c
kawashima <nazonohito51@hotmail.com>
2016-11-20 17:50:29 +0900

    create shared errors template

--- resources/views/tweet/edit.blade.php
+++ resources/views/tweet/edit.blade.php
@@ -6,15 +6,7 @@
 
 @section('content')
     @if (count($errors) > 0)
-        <div class="row">
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        </div>
+        @include('shared.errors')
     @endif
     <div class="row">
         <div class="col-md-12">
