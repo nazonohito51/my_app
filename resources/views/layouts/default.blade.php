592e85fde374a06809d9f3ef165e6e8a3a5027a7
kawashima <nazonohito51@hotmail.com>
2016-11-20 22:10:22 +0900

    create navbar

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,6 +7,7 @@
 </head>
 <body>
 <div class="container">
+    @include('shared.navigation')
     <div class="page-header">
         <h1>@yield('page-header')</h1>
     </div>
---
+++ resources/views/shared/navigation.blade.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="{{ route('tweet.index') }}" class="navbar-brand">マイアプリ</a>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+            <li>
+                <a href="{{ route('auth.get_login') }}">ログイン</a>
+            </li>
+        </ul>
+    </div>
+</nav>
