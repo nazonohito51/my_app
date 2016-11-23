38024b77a05489d66aa7086c3dca4653d24c86ca
kawashima <nazonohito51@hotmail.com>
2016-11-23 17:18:57 +0900

    fix auth check logic in navigation bar

--- resources/views/shared/navigation.blade.php
+++ resources/views/shared/navigation.blade.php
@@ -4,19 +4,18 @@
             <a href="{{ route('tweet.index') }}" class="navbar-brand">マイアプリ</a>
         </div>
         <ul class="nav navbar-nav navbar-right">
-                @if(Auth::check())
-                    <li>
-                        <a href="{{ route('auth.get_logout') }}">ログアウト</a>
-                    </li>
-                @else
-                    <li>
-                        <a href="{{ route('auth.get_register') }}">ユーザ新規登録</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('auth.get_login') }}">ログイン</a>
-                    </li>
-                @endif
-            </li>
+            @if(auth()->check())
+                <li>
+                    <a href="{{ route('auth.get_logout') }}">ログアウト</a>
+                </li>
+            @else
+                <li>
+                    <a href="{{ route('auth.get_register') }}">ユーザ新規登録</a>
+                </li>
+                <li>
+                    <a href="{{ route('auth.get_login') }}">ログイン</a>
+                </li>
+            @endif
         </ul>
     </div>
 </nav>
