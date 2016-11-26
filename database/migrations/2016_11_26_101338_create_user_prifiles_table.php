df48efdf2fe2ba5c34565a00b51f87e50fb3b65e
kawashima <nazonohito51@hotmail.com>
2016-11-26 20:01:47 +0900

    add user_profiles table

---
+++ database/migrations/2016_11_26_101338_create_user_prifiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserPrifilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('introduction');
+            $table->date('birthday');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_profiles');
+    }
+}
