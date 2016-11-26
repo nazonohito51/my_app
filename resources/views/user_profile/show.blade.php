b9d93fac66340fff50fac5659f4cda928d343156
kawashima <nazonohito51@hotmail.com>
2016-11-27 03:08:33 +0900

    fix user_production introduction escape

--- resources/views/user_profile/show.blade.php
+++ resources/views/user_profile/show.blade.php
@@ -10,12 +10,16 @@
             <h3>名前</h3>
             <p>{{ $user_profile->user->name }}</p>
             <h3>紹介文</h3>
-            <p>{{ $user_profile->introduction }}</p>
+            <p>{!! nl2br(e($user_profile->introduction)) !!}</p>
             <h3>誕生日</h3>
             <p>{{ $user_profile->birthday }}</p>
         </div>
-        @can('update-user_profile', $user_profile)
-            <a href="{!! route('user_profile.edit', ['id' => $user_profile->id]) !!}" class="btn btn-primary">編集</a>
-        @endcan
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            @can('update-user_profile', $user_profile)
+                <a href="{!! route('user_profile.edit', ['id' => $user_profile->id]) !!}" class="btn btn-primary">編集</a>
+            @endcan
+        </div>
     </div>
 @stop
