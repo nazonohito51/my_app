0186bc0ffc725e6d47e3dad716a1394a80bc601c
kawashima <nazonohito51@hotmail.com>
2016-11-26 16:28:14 +0900

    undisplay tweet control routing in not login

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -7,7 +7,9 @@
 @section('content')
     <div class="row">
         <div class="col-md-2">
-            <a href="{!! route('tweet.create') !!}" class="btn btn-primary">新しいツイートを投稿する</a>
+            @if(auth()->check())
+                <a href="{!! route('tweet.create') !!}" class="btn btn-primary">新しいツイートを投稿する</a>
+            @endif
         </div>
         <div class="col-md-10">
             <table class="table">
@@ -17,16 +19,18 @@
                         <td>
                             {{ $tweet->user->name }}: <a href="{!! route('tweet.show', ['id' => $tweet->id]) !!}">{{ $tweet->body }}</a>
                         </td>
-                        <td class="text-right">
-                            <a href="{!! route('tweet.edit', ['id' => $tweet->id]) !!}" class="btn btn-primary btn-sm">編集</a>
-                        </td>
-                        <td class="text-right">
-                            <form action="{{ route('tweet.destroy', ['id' => $tweet->id]) }}" method="post">
-                                <input type="hidden" name="_method" value="DELETE">
-                                {!! csrf_field() !!}
-                                <button type="submit" class="btn btn-danger btn-sm">削除</button>
-                            </form>
-                        </td>
+                        @if(auth()->check())
+                            <td class="text-right">
+                                <a href="{!! route('tweet.edit', ['id' => $tweet->id]) !!}" class="btn btn-primary btn-sm">編集</a>
+                            </td>
+                            <td class="text-right">
+                                <form action="{{ route('tweet.destroy', ['id' => $tweet->id]) }}" method="post">
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    {!! csrf_field() !!}
+                                    <button type="submit" class="btn btn-danger btn-sm">削除</button>
+                                </form>
+                            </td>
+                        @endif
                     </tr>
                 @endforeach
                 </tbody>
